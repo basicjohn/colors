b6b5ea068f59ed5a7077c6e5216e11b4817810be
basicjohn <edmondsonj@gmail.com>
2021-03-23 13:29:30 -0700

    added dark mode & light mode buttons
    co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $("button#green").click(function () {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function () {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function () {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  $("button#lightMode").click(function () {
+    $("body").removeClass();
+    $("body").addClass("lightMode-background");
+  });
+  $("button#darkMode").click(function () {
+    $("body").removeClass();
+    $("body").addClass("darkMode-background");
+  });
+});
